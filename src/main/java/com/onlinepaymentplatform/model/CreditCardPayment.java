ccb8746e5c53178e5cc1e868ca05ba766ed6838d
Nitishe27 <nitisheprem@gmail.com>
2025-11-19 15:29:47 +0530

    CreditCardPayment Commit

---
+++ src/main/java/com/onlinepaymentplatform/model/CreditCardPayment.java
@@ -0,0 +1,16 @@
+package com.onlinepaymentplatform.model;
+
+public class CreditCardPayment extends Payment{
+
+    private String cardNumber;
+
+    public CreditCardPayment(double amount, String cardNumber) {
+        super(amount);
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void processPayment(){
+        System.out.println("Processing credit card payment of $ "+ getAmount()+" using card" + cardNumber);
+    }
+}
