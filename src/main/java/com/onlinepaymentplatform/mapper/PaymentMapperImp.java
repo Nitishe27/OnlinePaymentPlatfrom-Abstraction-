86b2dfa539f65c14f55e7253cb8e221523e7955e
Nitishe27 <nitisheprem@gmail.com>
2025-11-19 15:25:59 +0530

    Payment Mapper Imp revise Commit

--- src/main/java/com/onlinepaymentplatform/mapper/PaymentMapperImp.java
+++ src/main/java/com/onlinepaymentplatform/mapper/PaymentMapperImp.java
@@ -1,5 +1,23 @@
 package com.onlinepaymentplatform.mapper;
+import com.onlinepaymentplatform.dto.PaymentRequest;
+import com.onlinepaymentplatform.entity.PaymentEntity;
+import org.springframework.stereotype.Component;
 
-public class PaymentMapperImp {
+@Component
+
+public class PaymentMapperImp implements PaymentMapper {
+
+    @Override
+    public PaymentEntity toEntity(PaymentRequest dto){
+        PaymentEntity entity = new PaymentEntity();
+
+        entity.setPaymentType(dto.getPaymentType());
+        entity.setAmount(dto.getAmount());
+        entity.setCardNumber(dto.getCardNumber());
+        entity.setCardHolder(dto.getCardHolder());
+        entity.setEmail(dto.getEmail());
+
+        return entity;
+    }
 
 }
