0fba4ac7cc7794d39d4a72599d78005d5a44f550
Nitishe27 <nitisheprem@gmail.com>
2025-11-19 15:25:38 +0530

    Payment Mapper revise Commit

--- src/main/java/com/onlinepaymentplatform/mapper/PaymentMapper.java
+++ src/main/java/com/onlinepaymentplatform/mapper/PaymentMapper.java
@@ -2,9 +2,6 @@ package com.onlinepaymentplatform.mapper;
 import com.onlinepaymentplatform.dto.PaymentRequest;
 import com.onlinepaymentplatform.entity.PaymentEntity;
 
-public class PaymentMapper {
-    PaymentEntity toEntity(PaymentRequest dto) {
-        return null;
-    }
-
+public interface PaymentMapper {
+    PaymentEntity toEntity(PaymentRequest dto);
 }
