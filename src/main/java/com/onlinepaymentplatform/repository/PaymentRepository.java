9b8eeac453b605852793e6d06c31ed72254b5be0
Nitishe27 <nitisheprem@gmail.com>
2025-11-19 16:23:07 +0530

    PaymentRepository Commit

--- src/main/java/com/onlinepaymentplatform/repository/PaymentRepository.java
+++ src/main/java/com/onlinepaymentplatform/repository/PaymentRepository.java
@@ -1,4 +1,9 @@
 package com.onlinepaymentplatform.repository;
 
-public class PaymentRepository {
+import com.onlinepaymentplatform.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long>{
 }
